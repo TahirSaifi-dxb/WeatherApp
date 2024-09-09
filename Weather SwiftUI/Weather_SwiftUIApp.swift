f64d42f7b98919b0e530c34c65c9e2be3f8d9fe7
Tahir Saifi <tahirhakim.52@me.com>
2024-09-09 10:19:03 +0400

    Initial Commit

---
+++ Weather SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather SwiftUI
+//
+//  Created by Tahir Saifi on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
